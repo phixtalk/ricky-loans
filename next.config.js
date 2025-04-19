03cf86f617e0b24ed111c9a0308e31f08ae9e354
phixtalk <ofoefulec_fny@yahoo.com>
2025-04-19 21:46:12 +0100

    Add deploy workflow for GitHub Pages again

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  trailingSlash: true,
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
